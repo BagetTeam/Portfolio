897f894bc0cb900fca45d8c5c7a1f75d29082007
Ewen <ewengue1432@gmail.com>
2025-12-06 01:04:18 -0500

    a

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="fixed top-0 left-0 z-10 p-6 bg-white/90 backdrop-blur shadow-md w-full">
+      <h1 className="text-3xl font-bold bg-linear-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+        My Creative Portfolio
+      </h1>
+    </header>
+  );
+}
